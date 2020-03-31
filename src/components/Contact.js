d68a4c23796ce5b6bef9812638e787f2d0c47e03
Alpasc <alinepascal@gmail.com>
2020-03-31 20:38:15 +0200

    fin quete

---
+++ src/components/Contact.js
@@ -0,0 +1,25 @@
+import React from 'react';
+
+function Contact() {
+  return (
+  <div className="Contact">
+    <img class="avatar" src="https://randomuser.me/api/portraits/men/35.jpg"
+         alt="user"
+    />
+    <div class="text">
+      <div class="name"> 
+        Logan Barnett
+      </div>
+      <div class="status">
+        <span class="status-online"></span>
+        <p class="status-text">online</p>
+      </div>
+    </div>
+  </div>
+  );
+}
+
+
+export default Contact;
+
+//
